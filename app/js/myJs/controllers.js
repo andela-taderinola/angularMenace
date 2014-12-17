67020b9c20648794aac2f2d6b14b723148a273bd
Isaac Ayodeji <deejecks@live.com>
2014-12-17 15:18:33 +0100

    http request made on page load

--- app/js/myJs/controllers.js
+++ app/js/myJs/controllers.js
@@ -6,12 +6,15 @@ gitUsersApp.controller('usersController', ['$scope', '$http', function(scope, ht
   scope.query = "andela";
   console.log(scope.query);
 
+  http({url: 'https://api.github.com/search/users', method: 'GET', params: {q: "andela"}}).success(function (data) {
+    scope.users = data.items;
+  });
+
   scope.userSearch = function () {
     http({url: 'https://api.github.com/search/users', method: 'GET', params: {q: scope.query}}).success(function (data) {
     scope.users = data.items;
     console.log(scope.users.length);
     console.log("clicked");
-    
     scope.query = "";
   });
   };
